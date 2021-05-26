9b815a8c25b7588c299a92034671c5e5b7df5085
ankit-sr <ankitrajput6622@gmail.com>
2021-05-26 15:53:22 +0530

    Initial Commit

---
+++ Student.java
@@ -0,0 +1,14 @@
+package p1;
+public class Student
+{
+ private int rollno;
+ private String name;
+ private int age;
+ public void setroll(int r) { rollno = r;}
+ public void setname(String n) { name = n; }
+ public void setage(int a) {age =a;}
+ public int getrollno() { return(rollno); }
+ public String getname() { return(name); }
+ public int getage() { return(age); }
+
+}
