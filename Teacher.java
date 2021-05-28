1f0d2b38364b1d56841c9b9022ae96f54d868ac5
ankit-sr <ankitrajput6622@gmail.com>
2021-05-28 19:25:14 +0530

    Initial Commit

---
+++ Teacher.java
@@ -0,0 +1,10 @@
+package p1;
+public class Teacher
+{
+ private String name;
+ private String subject;
+ public void setname(String n) { name =n; }
+ public void setsubject(String s) { subject =s; }
+ public String getname() { return (name); }
+ public String getsubject() { return (subject); }
+}
